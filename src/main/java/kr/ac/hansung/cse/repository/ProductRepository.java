5ad0f2a6bc68a1c6cdbda4e9771add8a21ec220f
YseungY <tmddusyy1115@naver.com>
2026-04-19 16:13:20 +0900

    feat: 상품 검색 JPQL 쿼리 추가

--- src/main/java/kr/ac/hansung/cse/repository/ProductRepository.java
+++ src/main/java/kr/ac/hansung/cse/repository/ProductRepository.java
@@ -131,4 +131,20 @@ public class ProductRepository {
             entityManager.remove(product);
         }
     }
+
+    public List<Product> findByNameContaining(String keyword) {
+        return em.createQuery(
+                        "SELECT p FROM Product p WHERE p.name LIKE :keyword",
+                        Product.class)
+                .setParameter("keyword", "%" + keyword + "%")
+                .getResultList();
+    }
+
+    public List<Product> findByCategoryId(Long categoryId) {
+        return em.createQuery(
+                        "SELECT p FROM Product p WHERE p.category.id = :cid",
+                        Product.class)
+                .setParameter("cid", categoryId)
+                .getResultList();
+    }
 }
